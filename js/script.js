5c6501b012f3bfaa20d6daee6d94f7cbb436e03a
lisaankerrasch <71253664+lisaankerrasch@users.noreply.github.com>
2021-02-16 11:44:59 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const url = "https://api.punkapi.com/v2/beers";
+const beerContainer = document.querySelector(".container");
+const loader = document.querySelector(".loader");
+
+async function getBeer() {
+  try {
+    const response = await fetch(url);
+    const list = await response.json();
+    const beer = list;
+
+    beerContainer.innerHTML = "";
+
+    for (let i = 0; i < beer.length; i++) {
+      beerContainer.innerHTML += `<a href="detail.html?id=${beer[i].id}">
+      <div class="beer">
+       <h1 class="name">${beer[i].name}</h1>
+      <p class="tagline">${beer[i].tagline}</p>
+      <img src="${beer[i].image_url}" alt="${beer[i].name}"/>
+     </div>
+      </a>`;
+      loader.classList.remove("loader");
+    }
+  } catch (error) {
+    console.log("error");
+    loader.classList.remove("loader");
+    beerContainer.innerHTML += `<div class="error"><p> Unable to fetch results </p></div>`;
+  }
+}
+
+getBeer();
