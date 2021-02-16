f90b07b83ee750e5d28e83cbdcceb48ef6a4078e
lisaankerrasch <lisa.ar.strom@gmail.com>
2021-02-16 13:36:47 +0100

    fixed classes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const url = "https://api.punkapi.com/v2/beers";
-const beerContainer = document.querySelector(".container");
+const beerContainer = document.querySelector(".beer-container");
 const loader = document.querySelector(".loader");
 
 async function getBeer() {
@@ -12,7 +12,7 @@ async function getBeer() {
 
     for (let i = 0; i < beer.length; i++) {
       beerContainer.innerHTML += `<a href="detail.html?id=${beer[i].id}">
-      <div class="beer">
+      <div class="beer-list">
        <h1 class="name">${beer[i].name}</h1>
       <p class="tagline">${beer[i].tagline}</p>
       <img src="${beer[i].image_url}" alt="${beer[i].name}"/>
